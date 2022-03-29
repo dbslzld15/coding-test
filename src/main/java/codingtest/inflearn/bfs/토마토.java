476a230c2725d1a85ed543eff6685858c7de96ef
dbslzld15 <dbslzld15@naver.com>
2022-03-29 21:42:39 +0900

    토마토 BFS 풀이 수정

--- src/main/java/codingtest/inflearn/bfs/토마토.java
+++ src/main/java/codingtest/inflearn/bfs/토마토.java
@@ -1,25 +1,35 @@
 package codingtest.inflearn.bfs;
 
-import codingtest.Main;
-
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class 토마토 {
-    static int[][] arr;
-    static int day = 1;
+    static int[][] arr, dis;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
 
     public void solution(int M, int N){
-        BFS(M, N);
-        int maxNum = -1;
+        boolean flag = true;
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
-                if(arr[i][j] > maxNum) maxNum = arr[i][j];
-                if(arr[i][j]==0) {
+                if (arr[i][j] == 0) {
+                    flag = false;
                     break;
+                }
+            }
+        }
+        if(flag) {
+            System.out.println(0);
+            return;
+        }
+        BFS(M, N);
+        int maxNum = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(dis[i][j] > maxNum) maxNum = dis[i][j];
+                if(arr[i][j]==0) {
+                    System.out.println(-1);
+                    return;
                 }
             }
         }
@@ -36,19 +46,16 @@ public class 토마토 {
         }
 
         while(!queue.isEmpty()){
-            int len = queue.size();
-            for(int i=0; i<len; i++){
-                Position pos = queue.poll();
-                for(int j=0; j<4; j++){
-                    int nx = pos.x + dx[j];
-                    int ny = pos.y + dy[j];
-                    if(nx>=0 && nx<N && ny>=0 && ny<M && arr[nx][ny]==0){
-                        arr[nx][ny] = day;
-                        queue.offer(new Position(nx, ny));
-                    }
+            Position pos = queue.poll();
+            for(int j=0; j<4; j++){
+                int nx = pos.x + dx[j];
+                int ny = pos.y + dy[j];
+                if(nx>=0 && nx<N && ny>=0 && ny<M && arr[nx][ny]==0){
+                    arr[nx][ny] = 1;
+                    dis[nx][ny] = dis[pos.x][pos.y] + 1;
+                    queue.offer(new Position(nx, ny));
                 }
             }
-            day++;
         }
     }
 
